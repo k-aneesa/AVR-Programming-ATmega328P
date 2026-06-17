28b190ac77fe14bb7aeaa47b8ddf50ce1c627628
k-aneesa <aneesajamshad2k17@gmail.com>
2026-06-17 14:49:18 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * cooling_system_with_LM35.c
+ *
+ * Created: 6/16/2026 3:03:17 PM
+ * Author : ANEESA
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
